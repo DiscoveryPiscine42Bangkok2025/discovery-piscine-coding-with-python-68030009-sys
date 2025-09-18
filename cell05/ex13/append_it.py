6aa22d3eaef95f60007d3598cafcecba2009333b
68030009-sys <68030009@kmitl.ac.th>
2025-09-18 20:19:28 +0700

    Update append_it.py

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,11 @@
-RTFM (Read the F-ing manual)
+import sys
+import re
+
+args sys.argv[1:]
+
+if not args:
+    print("none")
+else:
+    for word in args:
+        if not re.search(r'isms', word):
+            print (f"(word)ism")
