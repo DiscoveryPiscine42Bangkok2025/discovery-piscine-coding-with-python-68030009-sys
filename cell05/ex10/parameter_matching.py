1c4c840f800e19194f424e8be194168b13354ba5
68030009-sys <68030009@kmitl.ac.th>
2025-09-18 20:12:28 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,13 @@
-RTFM (Read the F-ing manual)
+#!/usr/bin/python3
+import sys #ต้องrunในbashผลถึงจะออกตามโจทย์
+
+if len(sys.argv) = 21:
+     parameter sys.argv[1]
+     user input = Input("What was the parameter? *)
+                        
+     If user input parameter:
+          print("Good job!")
+     else:
+          print("Nope, sorry...")
+else:
+    print("none"))
