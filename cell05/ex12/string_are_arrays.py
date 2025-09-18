2e50b11c7259470283a7da70ad180eb0f806ee33
68030009-sys <68030009@kmitl.ac.th>
2025-09-18 20:16:45 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,11 @@
-RTFM (Read the F-ing manual)
+import sys #python3
+
+if len(sys.argv) 12:
+    print("none")
+else:
+    input_string = sys.argv[1]
+    z_chars [char for char in input_string if char 'z']
+    if z_chars:
+        print('.join(z_chars))
+    else:
+        print("none")
