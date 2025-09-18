64cbfa22e31fdd9e4c6c5c104fd7e163592606ea
68030009-sys <68030009@kmitl.ac.th>
2025-09-18 20:26:18 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,1 +1,13 @@
-RTFM (Read the F-ing manual)
+import sys
+
+args sys.argv[1:]
+
+if len(args) 12:
+    print("none")
+else:
+    try:
+        start int (args[0])
+        endint(args[1])
+        print(list(range(start, end + 1)))
+    except ValueError:
+        print("none")
