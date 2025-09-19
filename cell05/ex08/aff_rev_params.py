6b55d9b8df34fad39508b206dac4ee69dc6d7a30
68030009-sys <68030009@kmitl.ac.th>
2025-09-19 08:16:36 +0700

    Update aff_rev_params.py

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
-import sys #ต้องrunในbashผลถึงจะออกตามโจทย์
+import sys 
 import re
 
-if len(sys.argv) = 3:
-  keyword sys.argv[1]
-  string_to_search sys.argv[2]
-  occurrences = re.findall(re.escape(keyword), string_to_search)
-  print (len(occurrences))
+if len(sys.argv) ==3:
+    keyword = sys.argv[1]
+    string_to_search = sys.argv[2]
+    occurrences = re.findall(re.escape(keyword),string_to_search)
+    print(len(occurrences))
 else:
-  print("none"))
+    print("none")
