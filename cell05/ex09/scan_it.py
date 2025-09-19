80d67eba8a07d3cf26a68295946218a54bc25237
68030009-sys <68030009@kmitl.ac.th>
2025-09-19 08:17:19 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
-import sys #ต้องrunในbashผลถึงจะออกตามโจทย์
-import re
+import sys
+import re 
 
-if len(sys.argv) == 3:
-    keyword sys.argv[1]
-    string_to_search sys.argv[2]
-    occurrences re.findall (keyword, string_to_search)
+if len(sys.argv) ==3:
+    keyword = sys.argv[1]
+    string_to_search = sys.argv[2]
+    occurrences = re.findall(keyword, string_to_search)
     print(len(occurrences))
 else:
-     print("none")
+    print("none")
